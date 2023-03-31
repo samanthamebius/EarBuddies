4be3bbe4bb43461387086a46e47ecd859b21aeaf
Ananya <aahl907@aucklanduni.ac.nz>
2023-03-31 14:28:49 +1300

    Initialised dropdown button

--- frontend/src/PageLayout.jsx
+++ frontend/src/PageLayout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './PageLayout.module.css';
+import icon from './assets/accountIconPLACEHOLDER.png';
 import logo from './assets/earBuddiesLogoWithName.png';
+import arrow from './assets/dropdownDownArrow.png';
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function PageLayout() {
@@ -11,13 +13,24 @@ export default function PageLayout() {
                 <Outlet />
             </div>
         </React.Fragment>
-    )
+    );
 }
 
 function NavMenu() {
     return (
         <header className={styles.navmenu}>
             <NavLink to="." ><img src={logo} className={styles.logo} /></NavLink>
+            <AccountDropdown></AccountDropdown>
         </header>
     );
+}
+
+function AccountDropdown() {
+    return (
+        <div className={styles.dropdown}>
+            <img src={icon} className={styles.icon}/>
+            <p>Name</p>
+            <img src={arrow} className={styles.arrow}/>
+        </div>
+    )
 }
