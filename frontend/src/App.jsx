ac9ebffb10a5b19fa03102a75dcd7a8b201f52ca
samanthamebius <smeb890@aucklanduni.ac.nz>
2023-03-31 15:16:00 +1300

    Add profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import PageLayout from './PageLayout';
 import HomePage from './HomePage';
+import ProfilePage from './ProfilePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<PageLayout/>}>
           <Route index element={<HomePage />} />
+          <Route path='profile' element={<ProfilePage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
