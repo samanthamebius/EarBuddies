7ecd41e3e5ddb8211295d01743a32403e795dd88
alor903 <alor903@aucklanduni.ac.nz>
2023-04-11 14:59:37 +1200

    created schema file for user and studio

--- backend/src/database/schema.js
+++ backend/src/database/schema.js
@@ -1,3 +1,26 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userId: {type: String, unique: true},
+    username: String,
+    profilePic: String,
+    userIsActive: Boolean,
+    userStudios: [{type: Schema.Types.ObjectId, ref: 'Studio'}]
+});
+
+const studioSchema = new Schema({
+    studioID: {type: Integer, unique: true},
+    studioName: String,
+    studioIsActive: Boolean,
+    studioUsers: [{type: Schema.Types.ObjectId, ref: 'User', required: true}],
+    studioHost: {type: Schema.Types.ObjectId, ref: 'User'},
+    studioGenres: [String],
+    studioPicture: String
+});
+
+const User = mongoose.model('User', userSchema);
+const Studio = mongoose.model('Studio', studioSchema);
+
+export {User, Studio};
