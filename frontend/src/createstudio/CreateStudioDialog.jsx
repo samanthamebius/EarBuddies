00fb1cc7253729b74a19e4cffda15176110937fb
arim402 <arim402@aucklanduni.ac.nz>
2023-04-11 19:44:46 +1200

    Button styling

--- frontend/src/createstudio/CreateStudioDialog.jsx
+++ frontend/src/createstudio/CreateStudioDialog.jsx
@@ -4,10 +4,11 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import styles from './CreateStudio.module.css';
+import styles from './CreateStudioDialog.module.css';
 import FileDropZone from "./FileDropZone";
 import ControlSwitch from "./ControlSwitch";
 import Tooltip from '@mui/material/Tooltip';
+import SearchBar from "../shared/SearchBar";
 
 export default function CreateStudioDialog({ isDialogOpened, handleCloseDialog }) {
     const handleClose = () => { handleCloseDialog(false) };
@@ -34,17 +35,18 @@ export default function CreateStudioDialog({ isDialogOpened, handleCloseDialog }
             <h2 className={styles.sectionHeading}>Genres</h2>
             <div className={styles.controlSection}>
                 <h2 className={styles.sectionHeading}>Only I Have Control</h2>
-                <Tooltip title="Only you will be able to queue, skip and pause songs." placement="right">
+                <Tooltip title="Only you will be able to queue, skip and pause songs." placement="right" arrow>
                     <div className={styles.switchContainer}>
                         <ControlSwitch/>
                     </div>
                 </Tooltip>
             </div>
             <h2 className={styles.sectionHeading}>Add Listeners</h2>
+            <SearchBar label={"Search using Spotify username ..."}/>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Create Studio</Button>
+        <DialogActions className={styles.buttons}>
+          <Button variant="contained" sx={{ color: '#606060'}} className={styles.cancelButton}onClick={handleClose}>Cancel</Button>
+          <Button variant="contained" className={styles.createButton} onClick={handleClose}>Create Studio</Button>
         </DialogActions>
       </Dialog>
     </div>
