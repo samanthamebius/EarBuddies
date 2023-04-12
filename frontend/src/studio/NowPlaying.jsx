a220e68e19452c54fdddc53e69d35b0400afcb70
samanthamebius <smeb890@aucklanduni.ac.nz>
2023-04-13 11:51:55 +1200

    Add song info elements

--- frontend/src/studio/NowPlaying.jsx
+++ frontend/src/studio/NowPlaying.jsx
@@ -5,14 +5,31 @@ import { useState } from 'react';
 import pause_btn from '../assets/now_playing/pause_btn.png'
 import play_btn from '../assets/now_playing/play_btn.png'
 
+import album_artwork from '../assets/now_playing/album_artwork_PLACEHOLDER.png'
+import artist_profile from '../assets/now_playing/artist_profile_PLACEHOLDER.png'
+
 export default function NowPlaying() {
   
   return (
     <div className={styles.nowplaying}>
       <label className={styles.greyheading}>Now Playing</label>
+      <SongInfo/>
       <ControlPanel/>
     </div>
   );
+}
+
+function SongInfo() {
+  return (
+    <div className={styles.songInfo}>
+      <img className={styles.albumArtwork} src={album_artwork}/>
+      <h1>champagne problems</h1>
+      <div>
+        <img src={artist_profile}/>
+        <h2>Taylor Swift</h2>
+      </div>
+    </div>
+  )
 }
 
 function ControlPanel() {
