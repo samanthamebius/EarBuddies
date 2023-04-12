39531c9a40fc6b846d243249666db63fd37a9eae
samanthamebius <smeb890@aucklanduni.ac.nz>
2023-04-13 11:26:20 +1200

    Add play button

--- frontend/src/studio/NowPlaying.jsx
+++ frontend/src/studio/NowPlaying.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import styles from './StudioPage.module.css'
+import { useState } from 'react';
+
+import pause_btn from '../assets/now_playing/pause_btn.png'
+import play_btn from '../assets/now_playing/play_btn.png'
 
 export default function NowPlaying() {
   
   return (
     <div className={styles.nowplaying}>
       <label className={styles.greyheading}>Now Playing</label>
+      <ControlPanel/>
     </div>
   );
+}
+
+function ControlPanel() {
+
+  const [isPlaying, setPlaying] = useState(false);
+
+  return (
+    <div>
+
+      <img className={styles.playBtn} src={isPlaying ? pause_btn : play_btn }
+            onClick={() => setPlaying(!isPlaying)} />
+    </div>
+  )
 }
