a467299c49e4135b283ded1a64888a475430c9be
azhe202 <azhe202@aucklanduni.ac.nz>
2023-04-18 20:12:33 +1200

    Basic styling for pinned messages

--- frontend/src/studio/chat/ChatMessage.jsx
+++ frontend/src/studio/chat/ChatMessage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ChatMessage.module.css";
 import messageDecoration1 from "../../assets/chat/messageDecoration1.svg";
 import messageDecoration2 from "../../assets/chat/messageDecoration2.svg";
+import pinIcon from "../../assets/chat/pinIcon.svg";
+import filledPinIcon from "../../assets/chat/filledPinIcon.svg";
 
-function ChatMessage({ newMessage }) {
+function ChatMessage(props) {
+	const { newMessage, setPinnedMessage } = props;
 	const { message, username } = newMessage;
+	const [isPinned, setIsPinned] = useState(false);
 	const isCurrentUser = username === "test";
 
 	const setMessageBodyStyle = () => {
@@ -14,7 +18,7 @@ function ChatMessage({ newMessage }) {
 			borderRadius: "16px",
 			overflowWrap: "break-word",
 			textAlign: "left",
-			maxWidth: "50%",
+			maxWidth: "100%",
 		};
 		return message;
 	};
@@ -30,16 +34,49 @@ function ChatMessage({ newMessage }) {
 		return message;
 	};
 
+	const handlePinMessage = () => {
+		setIsPinned(!isPinned);
+
+		if (isPinned) {
+			setPinnedMessage(message);
+		}
+	};
+
 	return (
 		<div style={setChatMessageStyle()}>
 			<h4 className={styles.username}>{username}</h4>
-			<div style={setMessageBodyStyle()}>
-				<p className={styles.body}>{message}</p>
+			<div
+				style={{
+					display: "flex",
+					// maxWidth: "60%",
+					flexDirection: `${isCurrentUser ? "row-reverse" : "row"}`,
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: `${isCurrentUser ? "flex-end" : "flex-start"}`,
+					}}
+				>
+					<div style={setMessageBodyStyle()}>
+						<p className={styles.body}>{message}</p>
+					</div>
+					<img
+						src={isCurrentUser ? messageDecoration1 : messageDecoration2}
+						style={{
+							margin: "-17px 0 0 -1px",
+							zIndex: 0,
+						}}
+					/>
+				</div>
+				<img
+					src={isPinned ? filledPinIcon : pinIcon}
+					alt="pin"
+					className={styles.pin}
+					onClick={() => handlePinMessage()}
+				/>
 			</div>
-			<img
-				src={isCurrentUser ? messageDecoration1 : messageDecoration2}
-				style={{ margin: "-17px 0 0 -1px", zIndex: 0 }}
-			/>
 		</div>
 	);
 }
