9d90769e6fc1e877a96fcc188711579de2a87035
arim402 <arim402@aucklanduni.ac.nz>
2023-04-20 20:40:21 +1200

    Remove duplicate files

--- frontend/src/GenreTag.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './GenreTag.module.css';
-
-export default function GenreTag({genre}) {
-    return (
-        <div className={styles.genreTag}>
-            <p className={styles.genreLabel}>{genre}</p>
-        </div>   
-    )
-}
