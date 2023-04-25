c0fc2250bc05f7ad794125d378ff8ec30cb41630
BreannaJury <79810883+bjur781@users.noreply.github.com>
2023-04-26 11:07:12 +1200

    backend

--- backend/src/routes/api/studio.js
+++ backend/src/routes/api/studio.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createStudio } from "../../database/studio_dao.js";
+import { createStudio, getStudio, deleteStudio } from "../../database/studio_dao.js";
 import { getUserId } from "../../database/user_dao.js";
 
 const router = express.Router();
 
+//create studio
 router.post("/new", async (req, res) => {
   try {
     const { name, listeners, host, genres, photo, isHostOnly } = req.body;
@@ -29,5 +30,34 @@ router.post("/new", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//get studio by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ msg: "No studio id provided" });
+    }
+    const studio = await getStudio(id);
+    res.json(studio);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//delete studio by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ msg: "No studio id provided" });
+    }
+    const studio = await deleteStudio(id);
+    res.json(studio);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 export default router;
