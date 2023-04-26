82c4d5d312a3df34ef57d4016f5c9375c8ed27a8
alor903 <alor903@aucklanduni.ac.nz>
2023-04-26 18:13:14 +1200

    got search results showing in frontend

--- backend/src/routes/dao/spotify_dao.js
+++ backend/src/routes/dao/spotify_dao.js
@@ -12,11 +12,43 @@ async function searchSpotify(query) {
     return new Promise((resolve, reject) => {
         thisSpotifyApi.search(query, ['track', 'episode', 'audiobook'])
             .then(function (data) {
-                console.log('Search by "Love"', data.body.tracks.items[0].album.images[0].url);
-                // want an image to show (from album)
-                // song name
-                // artist name
-                // spotify id
+                const results = [];
+                for (var i = 0; i < data.body.tracks.items.length; i++) {
+                    const track = {
+                        name: data.body.tracks.items[i].name,
+                        image: data.body.tracks.items[i].album.images[0].url,
+                        artists: [],
+                        id: data.body.tracks.items[i].id,
+                        type: "track"
+                    }
+                    for (var j = 0; j < data.body.tracks.items[i].artists.length; j++) {
+                        track.artists.push(data.body.tracks.items[i].artists[j].name);
+                    }
+                    results.push(track);
+                }
+                for (var i = 0; i < data.body.episodes.items.length; i++) {
+                    const episode = {
+                        name: data.body.episodes.items[i].name,
+                        image: data.body.episodes.items[i].images[0].url,
+                        id: data.body.episodes.items[i].id,
+                        type: "episode"
+                    }
+                    results.push(episode);
+                }
+                for (var i = 0; i < data.body.audiobooks.items.length; i++) {
+                    const audiobook = {
+                        name: data.body.audiobooks.items[i].name,
+                        image: data.body.audiobooks.items[i].images[0].url,
+                        id: data.body.audiobooks.items[i].id,
+                        authors: [],
+                        type: "audiobook"
+                    }
+                    for (var j = 0; j < data.body.audiobooks.items[i].authors.length; j++) {
+                        audiobook.authors.push(data.body.audiobooks.items[i].authors[j].name);
+                    }
+                    results.push(audiobook);
+                    resolve(results)
+                }
             })
             .catch(function (err) {
                 console.log("Something went wrong!", err);
