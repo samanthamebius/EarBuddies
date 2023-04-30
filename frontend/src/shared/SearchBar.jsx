a5d72942c75961f0a31788b0fd990d1100feb048
arim402 <arim402@aucklanduni.ac.nz>
2023-04-30 14:18:15 +1200

    Search bar theme

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,6 +1,7 @@
 // Credit to https://frontendshape.com/post/react-mui-5-search-bar-example
 
-import { Container, InputAdornment, TextField } from "@mui/material";
+import { Container, InputAdornment, TextField, styled } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from "react";
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import styles from './SearchBar.module.css';
@@ -9,14 +10,24 @@ import { sizing } from '@mui/system';
 function SearchBar({label}) {
   const [focused, setFocused] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-
+  
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: '#CA3FF3',
+      },
+    },
+  });
+
   return (
     <Container disableGutters={true} className={styles.searchBar}>
-      <TextField
+      <ThemeProvider theme={theme}>
+        <TextField
+        color="secondary"
         id="search"
         type="search"
         label={label}
@@ -33,8 +44,9 @@ function SearchBar({label}) {
         InputLabelProps={{
           shrink: focused || searchTerm.length > 0,
           style: {marginLeft: searchTerm || focused  ? 0 : 30 }
-        }}
-      />
+        }} />
+      </ThemeProvider>
+      
     </Container>
   );
 }
