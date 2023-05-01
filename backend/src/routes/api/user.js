3394e4635b26b25f81b6e715db48a0cb9c17be98
alor903 <alor903@aucklanduni.ac.nz>
2023-05-02 00:33:22 +1200

    added routes for last 2 search functions

--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { getUser, getUserbyId, searchStudios } from "../../dao/user_dao";
-import { getStudios } from "../../dao/studio_dao";
+import { getUser, getUserbyId, getUsers, searchActiveStudios, searchStudios } from "../../dao/user_dao";
 
 const router = express.Router();
 
@@ -20,18 +19,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/:id/studios", async (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    return res.status(400).json({ msg: "No user id provided" });
+router.get("/:username/studios", async (req, res) => {
+  const { username } = req.params;
+  if (!username) {
+    return res.status(400).json({ msg: "No username provided" });
   }
   try {
-    const studios = searchStudios(id);
+    const studios = searchStudios(username);
     res.json(studios);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.get("/:username/active", async (req, res) => {
+  const { username } = req.params;
+  if (!username) {
+    return res.status(400).json({ msg: "No username provided" });
+  }
+  try {
+    const studios = await searchActiveStudios(username);
+    res.json(studios);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("users", async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
