70c2fc2855ed223ba2c3ee78a18b89e7030053a7
BreannaJury <79810883+bjur781@users.noreply.github.com>
2023-05-06 11:34:55 +1200

    frontend

--- frontend/src/PageNotFound.jsx
+++ frontend/src/PageNotFound.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from './shared/ConfirmationDialog.module.css';
+import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
+import Button from '@mui/material/Button';
 
 function PageNotFound() {
     return (
-        <div>
-            <h1>Page Not Found</h1>
+        <div className={styles.spacing}>
+            <div className={styles.icon}>
+                <ErrorOutlineRoundedIcon fontSize={'large'} style={{ color: '#B03EEE'}}/>
+            </div>
+            <h1 style={{display: 'flex', justifyContent: 'center', fontSize: '26px'}} className={styles.sectionHeading}>
+                Oops! The page you're looking for doesn't exist
+            </h1>
+            <Button sx={{ fontWeight: 600, fontSize: '16px', marginTop: '15px' }} variant='contained' onClick={() => history.push('/home')}>Back to home</Button>
         </div>
+            
     );
 }
 
