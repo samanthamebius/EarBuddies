be2d817e3edfe448bd4c877a238b5f458202078d
BreannaJury <79810883+bjur781@users.noreply.github.com>
2023-05-06 11:36:18 +1200

    button redirect

--- frontend/src/PageNotFound.jsx
+++ frontend/src/PageNotFound.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from './shared/ConfirmationDialog.module.css';
 import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+    const navigate = useNavigate();
     return (
         <div className={styles.spacing}>
             <div className={styles.icon}>
@@ -12,7 +14,7 @@ function PageNotFound() {
             <h1 style={{display: 'flex', justifyContent: 'center', fontSize: '26px'}} className={styles.sectionHeading}>
                 Oops! The page you're looking for doesn't exist
             </h1>
-            <Button sx={{ fontWeight: 600, fontSize: '16px', marginTop: '15px' }} variant='contained' onClick={() => history.push('/home')}>Back to home</Button>
+            <Button sx={{ fontWeight: 600, fontSize: '16px', marginTop: '15px' }} variant='contained' onClick={() => navigate('/')}>Back to home</Button>
         </div>
             
     );
