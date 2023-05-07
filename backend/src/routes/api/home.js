2588c86adf26b384103e9c0714c915725256c3dd
samanthamebius <smeb890@aucklanduni.ac.nz>
2023-05-07 15:50:28 +1200

    Correct bug in endpoint

--- backend/src/routes/api/home.js
+++ backend/src/routes/api/home.js
@@ -16,7 +16,7 @@ router.get("/:id/studios", async (req, res) => {
         const studios = [];
         for (let i = 0; i < studioIds.length; i++) {
             const studio = await getStudio(studioIds[i]);
-            studios.push(studio);
+            studios.push(studio[0]);
         }
         return res.status(200).json(studios);
     } catch (err) {
