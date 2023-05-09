2370ee223639c5c2f143cd43c96eb0e3969a229f
samanthamebius <smeb890@aucklanduni.ac.nz>
2023-05-09 19:42:10 +1200

    Extract component to new file

--- frontend/src/createstudio/CreateStudioDialog.jsx
+++ frontend/src/createstudio/CreateStudioDialog.jsx
@@ -1,18 +1,16 @@
 import { TextField, Button, Dialog, DialogActions, DialogContent, Tooltip, List } from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import styles from './CreateStudioDialog.module.css';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import FileDropZone from "./FileDropZone";
 import ControlSwitch from "./ControlSwitch";
-import SearchBar from "../shared/SearchBar";
+import AddListenersBlock from "../shared/AddListenersBlock";
 import SelectedGenreTag from "./SelectedGenreTag";
 import UnselectedGenreTag from "./UnselectedGenreTag";
 import { styled } from '@mui/material/styles';
 import { tooltipClasses } from '@mui/material/Tooltip';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { ListItem, ListItemText, ListItemAvatar, Avatar, Menu, MenuItem } from "@mui/material";
-import ClearRounded from "@mui/icons-material/ClearRounded";
 
 function useStudioPost() {
   const postStudio = async (
@@ -250,100 +248,6 @@ export default function CreateStudioDialog({ isDialogOpened, handleCloseDialog }
           <Button sx={{ fontWeight: 600 }} variant="contained" className={styles.createButton} onClick={handleSubmit}>Create Studio</Button>
         </DialogActions>
       </Dialog>
-    </div>
-  );
-}
-
-function AddListenersBlock() {
-
-  const [listenerSearchResults, setListenerSearchResults] = useState([]);
-  const [displayedSearchResults, setDisplayedSearchResults] = useState([]);
-  const [listeners, setListeners] = useState([]);
-
-  useEffect(() => {
-    const difference = listenerSearchResults.filter(x => !listeners.some(y => y._id === x._id));
-    setDisplayedSearchResults(difference);
-  }, [listenerSearchResults, listeners])
-
-  function addListener(listener) {
-    const isFound = listeners.some(obj => {
-      if (obj === listener) {
-        return true;
-      }
-    });
-
-    if (!isFound) {
-      setListeners(oldListeners => [...oldListeners, listener]);
-    }
-  }
-
-  function removeListener(listener) {
-    setListeners((oldListeners) =>
-      oldListeners.filter((oldListener) => oldListener !== listener)
-    );
-  }
-
-  return (
-    <div>
-        {/* Add Listeners */}
-    <h2 className={styles.sectionHeading}>Add Listeners</h2>
-    <SearchBar
-      searchType={"users"}
-      label={"Search using Spotify username ..."}
-      studioId={""}
-      setResults={setListenerSearchResults}
-      studio={""} />
-
-    {/* Map search results */}
-    {displayedSearchResults.length > 0 ? <List className={styles.searchResults}>
-      {displayedSearchResults.map((listener, i) => (
-        <ListItem
-          key={i}
-          secondaryAction={
-            <Button
-              edge="end"
-              aria-label="more options"
-              sx={{ fontWeight: 600 }}
-              variant="contained"
-              onClick={() => addListener(listener)}>
-              Add
-            </ Button>
-          }>
-          <ListItemAvatar>
-            <Avatar>
-              <img className={styles.image} src={listener.profilePicture} />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={listener.userDisplayName} />
-        </ListItem>
-      ))}
-    </List> : null}
-
-    {/* Map Listeners */}
-    {listeners.length > 0 ?
-      <>
-        <h2 className={styles.sectionHeading}>Listeners</h2>
-        <List className={styles.listeners}>
-          {listeners.map((listener, i) => (
-            <ListItem
-              key={i}
-              secondaryAction={
-                <ClearRounded
-                  edge="end"
-                  style={{ color: "#757575" }}
-                  className={styles.clearIcon}
-                  onClick={() => removeListener(listener)} />
-              }>
-              <ListItemAvatar>
-                <Avatar>
-                  <img className={styles.image} src={listener.profilePicture} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={listener.userDisplayName} />
-            </ListItem>
-          ))}
-        </List>
-      </> : null}
     </div>
   );
 }
---
+++ frontend/src/shared/AddListenersBlock.jsx
@@ -0,0 +1,100 @@
+import {Button, List } from "@mui/material";
+import styles from '../createstudio/CreateStudioDialog.module.css';
+import React, { useEffect, useState } from "react";
+import SearchBar from "../shared/SearchBar";
+import { ListItem, ListItemText, ListItemAvatar, Avatar, Menu, MenuItem } from "@mui/material";
+import ClearRounded from "@mui/icons-material/ClearRounded";
+
+export default function AddListenersBlock() {
+
+  const [listenerSearchResults, setListenerSearchResults] = useState([]);
+  const [displayedSearchResults, setDisplayedSearchResults] = useState([]);
+  const [listeners, setListeners] = useState([]);
+
+  useEffect(() => {
+    const difference = listenerSearchResults.filter(x => !listeners.some(y => y._id === x._id));
+    setDisplayedSearchResults(difference);
+  }, [listenerSearchResults, listeners])
+
+  function addListener(listener) {
+    const isFound = listeners.some(obj => {
+      if (obj === listener) {
+        return true;
+      }
+    });
+
+    if (!isFound) {
+      setListeners(oldListeners => [...oldListeners, listener]);
+    }
+  }
+
+  function removeListener(listener) {
+    setListeners((oldListeners) =>
+      oldListeners.filter((oldListener) => oldListener !== listener)
+    );
+  }
+
+  return (
+    <div>
+        {/* Add Listeners */}
+    <h2 className={styles.sectionHeading}>Add Listeners</h2>
+    <SearchBar
+      searchType={"users"}
+      label={"Search using Spotify username ..."}
+      studioId={""}
+      setResults={setListenerSearchResults}
+      studio={""} />
+
+    {/* Map search results */}
+    {displayedSearchResults.length > 0 ? <List className={styles.searchResults}>
+      {displayedSearchResults.map((listener, i) => (
+        <ListItem
+          key={i}
+          secondaryAction={
+            <Button
+              edge="end"
+              aria-label="more options"
+              sx={{ fontWeight: 600 }}
+              variant="contained"
+              onClick={() => addListener(listener)}>
+              Add
+            </ Button>
+          }>
+          <ListItemAvatar>
+            <Avatar>
+              <img className={styles.image} src={listener.profilePicture} />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={listener.userDisplayName} />
+        </ListItem>
+      ))}
+    </List> : null}
+
+    {/* Map Listeners */}
+    {listeners.length > 0 ?
+      <>
+        <h2 className={styles.sectionHeading}>Listeners</h2>
+        <List className={styles.listeners}>
+          {listeners.map((listener, i) => (
+            <ListItem
+              key={i}
+              secondaryAction={
+                <ClearRounded
+                  edge="end"
+                  style={{ color: "#757575" }}
+                  className={styles.clearIcon}
+                  onClick={() => removeListener(listener)} />
+              }>
+              <ListItemAvatar>
+                <Avatar>
+                  <img className={styles.image} src={listener.profilePicture} />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={listener.userDisplayName} />
+            </ListItem>
+          ))}
+        </List>
+      </> : null}
+    </div>
+  );
+}
