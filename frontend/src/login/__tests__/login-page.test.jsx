c245827d70b1b75aaed54eb593b606bae371b2e2
arim402 <arim402@aucklanduni.ac.nz>
2023-05-09 16:43:41 +1200

    Create login-page test

---
+++ frontend/src/login/__tests__/login-page.test.jsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { expect, it } from 'vitest';
+import { render } from '@testing-library/react';
+import LogInPage from '../LogInPage';
+
+/**
+ * Tests that appropriate elements render on login page
+ */
+it('renders login page correctly', () => {
+    const { getByRole, getByText, getByAltText } = render(<LogInPage />);
+
+    expect(getByRole('button', { name: 'Log In Using Spotify' })).toBeInTheDocument();
+    expect(getByAltText('Ear Buddies Logo')).toBeInTheDocument();
+    expect(getByText('EAR BUDDIES')).toBeInTheDocument();
+});
