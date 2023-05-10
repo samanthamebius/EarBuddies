901d09e7bcabcc3bba1f1f179e0acba57d0e4b74
Ananya <aahl907@aucklanduni.ac.nz>
2023-05-10 19:49:07 +1200

    400 error code test

---
+++ frontend/src/__tests__/page-not-found.test.jsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom';
+import { expect, it } from 'vitest';
+import { getAllByTestId, getByRole, render } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import React from "react";
+import PageNotFound from '../PageNotFound';
+import localStorageMock from './local-storage-mock.js';
+
+const mockUser1 = {
+  _id: 1,
+  username: "testUser1",
+  userDisplayName: "testUser1",
+  spotifyPic: "testUser1",
+  profilePic: "testUser1",
+  userIsActive: false,
+  userStudios: [],
+};
+
+beforeAll(() => {
+  global.localStorage = localStorageMock;
+});
+
+
+it('page not found renders correctly for 400 code', () => {
+  localStorage.setItem('current_user_id', mockUser1._id);
+
+  const { getByTestId, getByText, getByRole } = render(
+    <MemoryRouter initialEntries={['/400']}>
+      <PageNotFound errorType={"400"}/>
+    </MemoryRouter>
+  )
+
+  expect(getByTestId('ErrorOutlineRoundedIcon')).toBeInTheDocument();
+  expect(getByText('Oops! An error occurred while communicating with Spotify')).toBeInTheDocument();
+  expect(getByRole('button', {name: "Please login again"})).toBeInTheDocument();
+});
+
+
