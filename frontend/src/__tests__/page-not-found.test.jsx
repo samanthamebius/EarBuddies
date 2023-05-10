6d8eeba6f497663de5fa69a40fa629c2a681ae47
Ananya <aahl907@aucklanduni.ac.nz>
2023-05-10 19:52:35 +1200

    404 error code test

--- frontend/src/__tests__/page-not-found.test.jsx
+++ frontend/src/__tests__/page-not-found.test.jsx
@@ -20,6 +20,19 @@ beforeAll(() => {
   global.localStorage = localStorageMock;
 });
 
+it('page not found renders correctly for 404 code', () => {
+    localStorage.setItem('current_user_id', mockUser1._id);
+  
+    const { getByTestId, getByText, getByRole } = render(
+      <MemoryRouter initialEntries={['/404']}>
+        <PageNotFound errorType={"404"}/>
+      </MemoryRouter>
+    )
+  
+    expect(getByTestId('ErrorOutlineRoundedIcon')).toBeInTheDocument();
+    expect(getByText('Oops! The page you\'re looking for doesn\'t exist')).toBeInTheDocument();
+    expect(getByRole('button', {name: "Back to home"})).toBeInTheDocument();
+});
 
 it('page not found renders correctly for 400 code', () => {
   localStorage.setItem('current_user_id', mockUser1._id);
