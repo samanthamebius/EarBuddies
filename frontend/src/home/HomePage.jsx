0ec5cb415bc8a62abf4aa202e31894a9d48f47bb
arim402 <arim402@aucklanduni.ac.nz>
2023-05-10 17:26:58 +1200

    Fix login test and searchbar test for home page

--- frontend/src/home/HomePage.jsx
+++ frontend/src/home/HomePage.jsx
@@ -9,74 +9,74 @@ import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import axios from "axios";
 
 function HomePage(props) {
-	const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-	const current_user = localStorage.getItem("current_user_id");
-	const id = JSON.parse(current_user);
-	const [studios, setStudios] = useState([]);
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const current_user = localStorage.getItem("current_user_id");
+    const id = JSON.parse(current_user);
+    const [studios, setStudios] = useState([]);
 
-	useEffect(() => {
-		const fetchStudios = async () => {
-			const response = await axios.get(`${BASE_URL}/api/home/${id}/studios`);
-			setStudios(response.data);
-			console.log("name " + studios[0].studioName);
-		}
-		fetchStudios();
-	},[]);
+    useEffect(() => {
+        const fetchStudios = async () => {
+            const response = await axios.get(`${BASE_URL}/api/home/${id}/studios`);
+            setStudios(response.data);
+            console.log("name " + studios[0].studioName);
+        }
+        fetchStudios();
+    }, []);
 
-	const { socket } = props;
-	const [isOpen, setIsOpen] = useState(false);
+    const { socket } = props;
+    const [isOpen, setIsOpen] = useState(false);
 
-	const handleOpen = () => {
-		setIsOpen(!isOpen);
-	};
+    const handleOpen = () => {
+        setIsOpen(!isOpen);
+    };
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.containerChild} style={{ marginRight: "45px" }}>
-				<div className={styles.header}>
-					<h1 className={styles.headings}>My Studios</h1>
-					<div className={styles.headerChild}>
-						<CreateStudioDialog
-							isDialogOpened={isOpen}
-							handleCloseDialog={() => setIsOpen(false)}
-						/>
-						<Button
-							sx={{ fontWeight: 600 }}
-							variant="contained"
-							size="large"
-							className={styles.button}
-							onClick={() => handleOpen()}
-						>
-							<AddRoundedIcon sx={{ pr: 1 }} /> Create Studio
-						</Button>
-					</div>
-				</div>
-				<SearchBar searchType={"studios"} label={"Search My Studios ..."} studioId={""} />
-				<div className={styles.cardContainer}>
-					{studios
-						.map((studio) => (
-							<StudioCard key={studio.studioName} socket={socket} studio={studio} />
-					))}
-				</div>
-			</div>
-			<div className={styles.containerChild}>
-				<div className={styles.header}>
-					<h1 className={styles.headings}>Listening Now</h1>
-					<div className={styles.headerChild}>
-						<img src={SoundWavesGradient} className={styles.soundWaves}></img>
-					</div>
-				</div>
-				<SearchBar searchType={"activeStudios"} label={"Search Studios Listening Now ..."} studioId={""} />
-				<div className={styles.cardContainer}>
-					{studios
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerChild} style={{ marginRight: "45px" }}>
+                <div className={styles.header}>
+                    <h1 className={styles.headings}>My Studios</h1>
+                    <div className={styles.headerChild}>
+                        <CreateStudioDialog
+                            isDialogOpened={isOpen}
+                            handleCloseDialog={() => setIsOpen(false)}
+                        />
+                        <Button
+                            sx={{ fontWeight: 600 }}
+                            variant="contained"
+                            size="large"
+                            className={styles.button}
+                            onClick={() => handleOpen()}
+                        >
+                            <AddRoundedIcon sx={{ pr: 1 }} /> Create Studio
+                        </Button>
+                    </div>
+                </div>
+                <SearchBar searchType={"studios"} label={"Search My Studios ..."} studioId={""} />
+                <div className={styles.cardContainer}>
+                    {studios
+                        .map((studio) => (
+                            <StudioCard key={studio.studioName} socket={socket} studio={studio} />
+                        ))}
+                </div>
+            </div>
+            <div className={styles.containerChild}>
+                <div className={styles.header}>
+                    <h1 className={styles.headings}>Listening Now</h1>
+                    <div className={styles.headerChild}>
+                        <img src={SoundWavesGradient} className={styles.soundWaves} alt="Pink Sound Waves"></img>
+                    </div>
+                </div>
+                <SearchBar searchType={"activeStudios"} label={"Search Studios Listening Now ..."} studioId={""} />
+                <div className={styles.cardContainer}>
+                    {studios
                         .filter((studio) => studio.studioIsActive === true)
                         .map((studio) => (
                             <StudioCard key={studio.studioName} socket={socket} studio={studio} />
                         ))}
 
-				</div>
-			</div>
-		</div>
-	);
+                </div>
+            </div>
+        </div>
+    );
 }
 export default HomePage;
