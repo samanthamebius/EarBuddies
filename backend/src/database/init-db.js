c67b550088d07771751ef4d1b7ac932a6d9fe255
alor903 <alor903@aucklanduni.ac.nz>
2023-05-11 17:25:53 +1200

    updated dummy usernames so that playlists can  be created when they are assigned host

--- backend/src/database/init-db.js
+++ backend/src/database/init-db.js
@@ -5,12 +5,12 @@ import mongoose from 'mongoose';
 import { User, Studio } from './schema';
 
 const users = [
-    { username: 'smeb123', userDisplayName: "Samantha Mebius", profilePic: "/src/assets/profile/anaconda.png", userIsActive: true, userStudios: [] },
-    { username: 'bre123', userDisplayName: "Breanna Jury", profilePic: "/src/assets/profile/snail.png", userIsActive: false, userStudios: [] },
-    { username: 'ananya456', userDisplayName: "Ananya Ahluwalia", profilePic: "/src/assets/profile/bunny.png", userIsActive: true, userStudios: [] },
-    { username: 'amy456', userDisplayName: "Amy Rimmer", profilePic: "/src/assets/profile/clown-fish.png", userIsActive: false, userStudios: [] },
-    { username: 'ange789', userDisplayName: "Angela Lorusso", profilePic: "/src/assets/profile/panda.png", userIsActive: true, userStudios: [] },
-    { username: 'yuewen789', userDisplayName: "Yuewen Zheng", profilePic: "/src/assets/profile/giraffe.png", userIsActive: false, userStudios: [] }
+    { username: 'samzieskates12', userDisplayName: "Samantha Mebius", profilePic: "/src/assets/profile/anaconda.png", userIsActive: true, userStudios: [] },
+    { username: 'brejury', userDisplayName: "Breanna Jury", profilePic: "/src/assets/profile/snail.png", userIsActive: false, userStudios: [] },
+    { username: 'ananya2001', userDisplayName: "Ananya Ahluwalia", profilePic: "/src/assets/profile/bunny.png", userIsActive: true, userStudios: [] },
+    { username: 'amy.rimmer01', userDisplayName: "Amy Rimmer", profilePic: "/src/assets/profile/clown-fish.png", userIsActive: false, userStudios: [] },
+    { username: 'anglorusso11', userDisplayName: "Angela Lorusso", profilePic: "/src/assets/profile/panda.png", userIsActive: true, userStudios: [] },
+    { username: '1236847493', userDisplayName: "Yuewen Zheng", profilePic: "/src/assets/profile/giraffe.png", userIsActive: false, userStudios: [] }
 ];
 
 
