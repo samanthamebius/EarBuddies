5fb3143ca1d6df181e81b436ca44c1212404773b
alor903 <alor903@aucklanduni.ac.nz>
2023-05-12 17:26:41 +1200

    updated init-db and comment

--- backend/src/database/init-db.js
+++ backend/src/database/init-db.js
@@ -4,31 +4,66 @@ dotenv.config();
 import mongoose from 'mongoose';
 import { User, Studio } from './schema';
 
+//dummy user data for testing
 const users = [
-    { username: 'samzieskates12', userDisplayName: "Samantha Mebius", profilePic: "/src/assets/profile/penguin.png", userIsActive: true, userStudios: [] },
-    { username: 'brejury', userDisplayName: "Breanna Jury", profilePic: "/src/assets/profile/snail.png", userIsActive: false, userStudios: [] },
-    { username: 'ananya2001', userDisplayName: "Ananya Ahluwalia", profilePic: "/src/assets/profile/bunny.png", userIsActive: true, userStudios: [] },
-    { username: 'amy.rimmer01', userDisplayName: "Amy Rimmer", profilePic: "/src/assets/profile/clown-fish.png", userIsActive: false, userStudios: [] },
-    { username: 'anglorusso11', userDisplayName: "Angela Lorusso", profilePic: "/src/assets/profile/panda.png", userIsActive: true, userStudios: [] },
-    { username: '1236847493', userDisplayName: "Yuewen Zheng", profilePic: "/src/assets/profile/giraffe.png", userIsActive: false, userStudios: [] }
+	{
+		username: 'sam123',
+		userDisplayName: 'Samantha Mebius',
+		profilePic: '/src/assets/profile/penguin.png',
+		userIsActive: true,
+		userStudios: [],
+	},
+	{
+		username: 'bre456',
+		userDisplayName: 'Breanna Jury',
+		profilePic: '/src/assets/profile/snail.png',
+		userIsActive: false,
+		userStudios: [],
+	},
+	{
+		username: 'ananya789',
+		userDisplayName: 'Ananya Ahluwalia',
+		profilePic: '/src/assets/profile/bunny.png',
+		userIsActive: true,
+		userStudios: [],
+	},
+	{
+		username: 'amy123',
+		userDisplayName: 'Amy Rimmer',
+		profilePic: '/src/assets/profile/clown-fish.png',
+		userIsActive: false,
+		userStudios: [],
+	},
+	{
+		username: 'ange456',
+		userDisplayName: 'Angela Lorusso',
+		profilePic: '/src/assets/profile/panda.png',
+		userIsActive: true,
+		userStudios: [],
+	},
+	{
+		username: 'yuewen789',
+		userDisplayName: 'Yuewen Zheng',
+		profilePic: '/src/assets/profile/giraffe.png',
+		userIsActive: false,
+		userStudios: [],
+	},
 ];
-
 
 main();
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
-    console.log('Connected to database!');
-    console.log();
+	await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+	console.log('Connected to database!');
+	console.log();
 
-    await User.deleteMany({});
-    await Studio.deleteMany({});
-    console.log('Deleted all users and studios!');
+	await User.deleteMany({});
+	await Studio.deleteMany({});
+	console.log('Deleted all users and studios!');
 
-    await User.insertMany(users);
-    console.log('Inserted new users!');
+	await User.insertMany(users);
+	console.log('Inserted new users!');
 
-    await mongoose.disconnect();
-    console.log('Disconnected from database!');
+	await mongoose.disconnect();
+	console.log('Disconnected from database!');
 }
-
