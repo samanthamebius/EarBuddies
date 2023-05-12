f7ea1ff671b1093fda34251d23579ac241e1ef83
arim402 <arim402@aucklanduni.ac.nz>
2023-05-12 13:00:44 +1200

    Fix dropzone and error pages

--- frontend/src/createstudio/FileDropZone.jsx
+++ frontend/src/createstudio/FileDropZone.jsx
@@ -6,77 +6,77 @@ import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
 import ImageRoundedIcon from '@mui/icons-material/ImageRounded';
 
 export default function FileDropZone(props) {
-    const [files, setFiles] = useState([]);
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
-        maxFiles: 1,
-        accept: { 'image/jpeg': [], 'image/png': [] },
-        onDrop: (acceptedFiles) => {
-            setFiles(
-                acceptedFiles.map((file) =>
-                    Object.assign(file, {
-                        preview: URL.createObjectURL(file),
-                    })
-                )
-            );
-            props.onFileChange(acceptedFiles);
-        },
-    });
+	const [files, setFiles] = useState([]);
+	const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+		maxFiles: 1,
+		accept: { 'image/jpeg': [], 'image/png': [] },
+		onDrop: (acceptedFiles) => {
+			setFiles(
+				acceptedFiles.map((file) =>
+					Object.assign(file, {
+						preview: URL.createObjectURL(file),
+					})
+				)
+			);
+			props.onFileChange(acceptedFiles);
+		},
+	});
 
-    const thumbs = files.map((file) => (
-        <div key={file.name}>
-            <div>
-                <img
-                    className={styles.previewImg}
-                    src={file.preview}
-                    onLoad={() => {
-                        URL.revokeObjectURL(file.preview);
-                    }}
-                />
-            </div>
-        </div>
-    ));
+	const thumbs = files.map((file) => (
+		<div key={file.name}>
+			<div>
+				<img
+					className={styles.previewImg}
+					src={file.preview}
+					onLoad={() => {
+						URL.revokeObjectURL(file.preview);
+					}}
+				/>
+			</div>
+		</div>
+	));
 
-    useEffect(() => {
-        return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
-    }, []);
+	useEffect(() => {
+		return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+	}, []);
 
-    const acceptedFileItems = acceptedFiles.map((file) => (
-        <p className={styles.uploadedfileText}>{file.path}</p>
-    ));
+	const acceptedFileItems = acceptedFiles.map((file) => (
+		<p className={styles.uploadedfileText}>{file.path}</p>
+	));
 
-    return (
-        <section className='container'>
-            <div {...getRootProps({ className: 'dropzone' })}>
-                <div className={styles.dropBox}>
-                    <div
-                        style={{ display: acceptedFiles.length == 0 ? '' : 'none' }}
-                        className={styles.uploadView}>
-                        <input {...getInputProps()} />
-                        <ImageRoundedIcon
-                            className={styles.photoIcon}
-                            color='disabled'
-                        />
-                        <p style={{ color: 'var(--headingColor)' }}>
-                            <span className={styles.focusText}>Upload a file</span> or
-                            drag and drop
-                        </p>
-                        <p style={{ margin: '0px', color: 'var(--headingColor)' }}>
-                            (Only 1 *.jpeg or *.png image will be accepted)
-                        </p>
-                    </div>
-                    <div
-                        style={{ display: acceptedFiles.length == 0 ? 'none' : '' }}
-                        className={styles.uploadedView}>
-                        <input {...getInputProps()} />
-                        {thumbs}
-                        {acceptedFileItems}
-                        <ClearRoundedIcon
-                            className={styles.closeIcon}
-                            style={{ color: 'var(--iconColor)' }}
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+	return (
+		<section className='container'>
+			<div {...getRootProps({ className: 'dropzone' })}>
+				<div className={styles.dropBox}>
+					<div
+						style={{ display: acceptedFiles.length == 0 ? '' : 'none' }}
+						className={styles.uploadView}>
+						<input {...getInputProps()} />
+						<ImageRoundedIcon
+							className={styles.photoIcon}
+							style={{ color: 'var(--iconColor)' }}
+						/>
+						<p style={{ color: 'var(--headingColor)' }}>
+							<span className={styles.focusText}>Upload a file</span> or
+							drag and drop
+						</p>
+						<p style={{ margin: '0px', color: 'var(--headingColor)' }}>
+							(Only 1 *.jpeg or *.png image will be accepted)
+						</p>
+					</div>
+					<div
+						style={{ display: acceptedFiles.length == 0 ? 'none' : '' }}
+						className={styles.uploadedView}>
+						<input {...getInputProps()} />
+						{thumbs}
+						{acceptedFileItems}
+						<ClearRoundedIcon
+							className={styles.closeIcon}
+							style={{ color: 'var(--iconColor)' }}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
