5c03c0804999b80abc9dd20e60651aade176fcae
alor903 <alor903@aucklanduni.ac.nz>
2023-05-12 22:37:55 +1200

    spotify dao done

--- backend/src/dao/spotify_dao.js
+++ backend/src/dao/spotify_dao.js
@@ -4,14 +4,29 @@ dotenv.config();
 
 var spotifyApi = null;
 
+/**
+ * set the spotify api from login
+ * @param api
+ */
 function setSpotifyApi(api) {
 	spotifyApi = api;
 }
 
+/**
+ * get spotify api to be used in routes
+ * @returns spotify api object
+ */
 function getSpotifyApi() {
 	return spotifyApi;
 }
 
+/**
+ * search spotify for a track or episode
+ * @param query
+ * @param thisSpotifyApi
+ * @param playlist
+ * @returns list of search results
+ */
 async function searchSpotify(query, thisSpotifyApi, playlist) {
 	const tracks = [];
 	thisSpotifyApi.getPlaylistTracks(playlist).then(function (data) {
@@ -28,6 +43,7 @@ async function searchSpotify(query, thisSpotifyApi, playlist) {
 				for (var i = 0; i < data.body.tracks.items.length; i++) {
 					// check if the track name starts with the query term
 					if (regex.test(data.body.tracks.items[i].name)) {
+						// get the track info
 						const track = {
 							name: data.body.tracks.items[i].name,
 							image: data.body.tracks.items[i].album.images[0].url,
@@ -35,6 +51,8 @@ async function searchSpotify(query, thisSpotifyApi, playlist) {
 							id: data.body.tracks.items[i].id,
 							type: 'track',
 						};
+
+						// get the artists of the track in a nice format with spacing
 						for (
 							var j = 0;
 							j < data.body.tracks.items[i].artists.length;
@@ -42,21 +60,30 @@ async function searchSpotify(query, thisSpotifyApi, playlist) {
 						) {
 							track.artists.push(data.body.tracks.items[i].artists[j].name);
 						}
+
+						//don't add the track if it's already in the playlist
 						if (!tracks.includes(data.body.tracks.items[i].uri)) {
 							results.push(track);
 						}
 					}
 				}
+
+				// do the same for episodes
 				for (var i = 0; i < data.body.episodes.items.length; i++) {
 					// check if the episode name starts with the query term
 					if (regex.test(data.body.episodes.items[i].name)) {
+						// get episode info
 						var episode = {
 							name: data.body.episodes.items[i].name,
 							image: data.body.episodes.items[i].images[0].url,
 							id: data.body.episodes.items[i].id,
 							type: 'episode',
 						};
-						results.push(episode);
+
+						//don't add the episode if it's already in the playlist
+						if (!tracks.includes(data.body.episodes.items[i].uri)) {
+							results.push(episode);
+						}
 					}
 				}
 				resolve(results);
@@ -68,6 +95,11 @@ async function searchSpotify(query, thisSpotifyApi, playlist) {
 	});
 }
 
+/**
+ * get the current track id
+ * @param thisSpotifyApi
+ * @returns track id
+ */
 async function getCurrentTrackId(thisSpotifyApi) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getMyCurrentPlayingTrack({ additional_types: 'episode' }).then(
@@ -82,6 +114,11 @@ async function getCurrentTrackId(thisSpotifyApi) {
 	});
 }
 
+/**
+ * get the current track
+ * @param thisSpotifyApi
+ * @returns track object
+ */
 async function getCurrentTrack(thisSpotifyApi) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getMyCurrentPlayingTrack({ additional_types: 'episode' }).then(
@@ -96,6 +133,12 @@ async function getCurrentTrack(thisSpotifyApi) {
 	});
 }
 
+/**
+ * get the current track artist
+ * @param thisSpotifyApi
+ * @param artist_id
+ * @returns artist object
+ */
 async function getArtist(artist_id, thisSpotifyApi) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getArtist(artist_id).then(
@@ -110,6 +153,12 @@ async function getArtist(artist_id, thisSpotifyApi) {
 	});
 }
 
+/**
+ * find the last track/episode in the playlist
+ * @param thisSpotifyApi
+ * @param playlist_id
+ * @returns track id that is last in the playlist
+ */
 async function getLastPlaylistTrackId(thisSpotifyApi, playlist_id) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getPlaylistTracks(playlist_id).then(
@@ -124,10 +173,17 @@ async function getLastPlaylistTrackId(thisSpotifyApi, playlist_id) {
 	});
 }
 
+/**
+ * get the current playback state
+ * @param thisSpotifyApi
+ * @param deviceId
+ * @returns true if playing on the device, false if not
+ */
 async function getPlaybackState(thisSpotifyApi, deviceId) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getMyCurrentPlaybackState({ additional_types: 'episode' }).then(
 			function (data) {
+				// check that there is a response and that the device id matches
 				if (data.statusCode == 200 && data.body.device.id == deviceId) {
 					resolve(true);
 				} else {
@@ -142,6 +198,11 @@ async function getPlaybackState(thisSpotifyApi, deviceId) {
 	});
 }
 
+/**
+ * create a new playlist for the studio
+ * @param studio
+ * @returns the new playlist id
+ */
 async function createNewStudioPlaylist(studio) {
 	const playlist_name = 'Earbuddies - ' + studio[0].studioName;
 	const api = getSpotifyApi();
@@ -155,10 +216,14 @@ async function createNewStudioPlaylist(studio) {
 	return createPlaylistRes.body.id;
 }
 
+/**
+ * copy over playlist tracks from old playlist to new playlist
+ * @param old_playlist
+ * @param new_playlist
+ */
 async function copyPlaylist(old_playlist, new_playlist) {
 	const api = getSpotifyApi();
 	if (!api) {
-		console.log('No Spotify API connection');
 		return res.status(403).json({ msg: 'No Spotify API connection' });
 	}
 	api.getPlaylistTracks(old_playlist).then(function (data) {
@@ -168,9 +233,7 @@ async function copyPlaylist(old_playlist, new_playlist) {
 		}
 		if (tracks.length > 0) {
 			api.addTracksToPlaylist(new_playlist, tracks).then(
-				function (data) {
-					console.log('Added tracks to playlist!');
-				},
+				function (data) {},
 				function (err) {
 					console.log('Something went wrong!', err);
 				}
@@ -179,21 +242,30 @@ async function copyPlaylist(old_playlist, new_playlist) {
 	});
 }
 
+/**
+ * changes playlist to new playlist and update host in db
+ * @param studio_id
+ * @param host
+ * @returns new host
+ */
 async function transferPlaylist(studio_id, host) {
-	console.log('Transfering playlist');
 	const studio = await getStudio(studio_id);
 	const old_playlist = studio[0].studioPlaylist;
 	const new_playlist = await createNewStudioPlaylist(studio);
-	console.log(new_playlist);
 	await copyPlaylist(old_playlist, new_playlist);
 	await updateStudioPlaylist(studio_id, new_playlist);
-	console.log('set new host ' + host);
 	await updateStudioHost(studio_id, host);
-	console.log(await getStudio(studio_id));
 	const updated_studio = await getStudio(studio_id);
 	return updated_studio.studioHost;
 }
 
+/**
+ * add track to playlist
+ * @param playlist_id
+ * @param track_id
+ * @param type
+ * @param thisSpotifyApi
+ */
 async function addPlaylistTrackAndQueue(playlist_id, track_id, type, thisSpotifyApi) {
 	thisSpotifyApi
 		.addTracksToPlaylist(playlist_id, ['spotify:' + type + ':' + track_id])
@@ -205,6 +277,14 @@ async function addPlaylistTrackAndQueue(playlist_id, track_id, type, thisSpotify
 		);
 }
 
+/**
+ * remove a track from the playlist
+ * @param playlist_id
+ * @param track_id
+ * @param snapshot_id
+ * @param type
+ * @param thisSpotifyApi
+ */
 async function removePlaylistTrack(
 	playlist_id,
 	track_id,
@@ -226,6 +306,12 @@ async function removePlaylistTrack(
 		);
 }
 
+/**
+ * get the playlist object
+ * @param playlist_id
+ * @param thisSpotifyApi
+ * @returns playlist object
+ */
 async function getPlaylist(playlist_id, thisSpotifyApi) {
 	return new Promise((resolve, reject) => {
 		thisSpotifyApi.getPlaylist(playlist_id).then(
@@ -239,6 +325,11 @@ async function getPlaylist(playlist_id, thisSpotifyApi) {
 	});
 }
 
+/**
+ * resume playing spotify
+ * @param thisSpotifyApi
+ * @param deviceId
+ */
 async function resumeSpotify(thisSpotifyApi, deviceId) {
 	thisSpotifyApi.play({ device_id: deviceId }).then(
 		function () {
@@ -255,6 +346,12 @@ async function resumeSpotify(thisSpotifyApi, deviceId) {
 	);
 }
 
+/**
+ * play a spotify playlist from beginning
+ * @param thisSpotifyApi
+ * @param uri
+ * @param deviceId
+ */
 async function playSpotify(thisSpotifyApi, uri, deviceId) {
 	thisSpotifyApi
 		.play({
@@ -277,17 +374,25 @@ async function playSpotify(thisSpotifyApi, uri, deviceId) {
 		);
 }
 
+/**
+ * pause spotify
+ * @param deviceId
+ * @param thisSpotifyApi
+ */
 async function pauseSpotify(deviceId, thisSpotifyApi) {
 	thisSpotifyApi.pause({ device_id: deviceId }).then(
-		function () {
-			console.log('Paused track!');
-		},
+		function () {},
 		function (err) {
 			console.log('Something went wrong!', err);
 		}
 	);
 }
 
+/**
+ * skip to next track
+ * @param thisSpotifyApi
+ * @param deviceId
+ */
 async function skipNext(thisSpotifyApi, deviceId) {
 	thisSpotifyApi.skipToNext({ device_id: deviceId }).then(
 		function () {},
@@ -297,6 +402,11 @@ async function skipNext(thisSpotifyApi, deviceId) {
 	);
 }
 
+/**
+ * skip to previous track
+ * @param thisSpotifyApi
+ * @param deviceId
+ */
 async function skipPrevious(thisSpotifyApi, deviceId) {
 	thisSpotifyApi.skipToPrevious({ device_id: deviceId }).then(
 		function () {},
